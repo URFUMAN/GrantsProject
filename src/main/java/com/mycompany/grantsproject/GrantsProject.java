d207b2cb27c74303f7f9f826df73a854f0796ba1
VasiliChernykh <chernykhvasilii@gmail.com>
2026-04-28 20:33:29 +0500

    Настройка подключения к БД и создание БД.

--- src/main/java/com/mycompany/grantsproject/GrantsProject.java
+++ src/main/java/com/mycompany/grantsproject/GrantsProject.java
@@ -8,6 +8,15 @@ public class GrantsProject {
 
         String path = "Гранты.csv";
 
+       List<Grant> grants = CSVReaderService.readGrants(path);
 
+        for (int i = 0; i < 10; i++) {
+            System.out.println(grants.get(i));
+        }
+
+        System.out.println("Всего записей: " + grants.size());
+        
+        DatabaseManager.connect();
+        DatabaseManager.createTables();
     }
 }
