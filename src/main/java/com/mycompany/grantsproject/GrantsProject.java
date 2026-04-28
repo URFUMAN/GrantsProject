23d29ddedb17ecf784fb6500829676641ebe7b67
VasiliChernykh <chernykhvasilii@gmail.com>
2026-04-28 20:27:12 +0500

    Init. Создан проект. Начат главный файл.

---
+++ src/main/java/com/mycompany/grantsproject/GrantsProject.java
@@ -0,0 +1,13 @@
+package com.mycompany.grantsproject;
+
+import java.util.List;
+
+public class GrantsProject {
+
+    public static void main(String[] args) {
+
+        String path = "Гранты.csv";
+
+
+    }
+}
